3e09731681f6f2ab13544eb84c377fdab67dfa8a
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2021-01-11 14:27:13 +0200

    add dorf1.php
    From the link: https://www.autoitscript.com/forum/applications/core/interface/file/attachment.php?id=10700

---
+++ Attachments/10K-11K/1-1000/10700/dorf1.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd"><html>
+<head>
+<title>Travian</title>
+<link rel=stylesheet type="text/css" href="un3.css">
+<script src="en3.js" type=text/javascript></script>
+
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+<body onload="start()"><div style="position:absolute; z-index:10; left:188px; top:0px;  height:60px;"><iframe  allowtransparency="true" scrolling="no" hspace="0" vspace="0" frameborder="0" marginheight="0" marginwidth="0" src="ad/un/ads.php" width="468" height="60"></iframe></div><div style="position:absolute; background-color:#747273; left:0px; top:0px; width:100%; height:60px;">
+<table cellspacing="0" cellpadding="0" width="100%" bgcolor="#747273">
+<tr>
+<td width="780" style="background-image:url(img/un/l/ad1.jpg); background-repeat: no-repeat;"></td>
+<td align="right"><img src="img/un/l/ad2.jpg" width="151" height="70" border="0"></td>
+</tr>
+</table>
+</div>
+<div style="position:relative; width:100%; left:0px; top:42px;">
+<div style="position:absolute; width:100%; height:100px; z-index:1; background-image:url(img/un/l/mw.gif); left: 0px; top: 0px;"></div><map name="nb"><area shape=rect coords="0,0,35,100" href="berichte.php" title="Reports"><area shape=rect coords="35,0,70,100" href="nachrichten.php" title="Messages"></map><div class="div2">
+<a href="dorf1.php" id="n1"><img class="fl2" src="img/un/l/d1.gif" width="70" height="100" border="0" onmouseover="this.className='fl1'" onmouseout="this.className='fl2'" title="Village overview"></a><a href="dorf2.php" id="n2"><img class="fl2" src="img/un/l/d2.gif" width="70" height="100" border="0" onmouseover="this.className='fl1'" onmouseout="this.className='fl2'" title="Village center"></a><a href="karte.php" id="n3"><img class="fl2" src="img/un/l/ka.gif" width="70" height="100" border="0" onmouseover="this.className='fl1'" onmouseout="this.className='fl2'" title="Map"></a><a href="statistiken.php" id="n4"><img class="fl2" src="img/un/l/st.gif" width="70" height="100" border="0" onmouseover="this.className='fl1'" onmouseout="this.className='fl2'" title="Statistics"></a><img src="img/un/l/m4.gif" width="70" height="100" border="0" usemap="#nb">
+</div>
+
+<div class="plus"><a href="plus.php"><img src="img/en/a/plus.gif" width="80" height="100" border="0" title="Plus menu"></a></div>
+<script type="text/javascript" language="javascript">
+function setCookie(name,value, time) {
+        expires = new Date();
+        //expires.setTime(expires.getTime() + (1000 * 86400 * 365));
+        expires.setTime(expires.getTime() + (1000 * time));
+        document.cookie = name + "="+value+"; expires=" + expires.toGMTString() +  "; path=/";
+}
+function GNButtonClick() {
+	setCookie('GNButton','1',86400);
+}
+</script>
+
+
+<img src="img/un/a/x.gif" width="1" height="90" border="0">
+
+<table cellspacing="0" cellpadding="0"><tr valign="top">
+<td class="s1">
+<a href="http://www.travian.com/"><img src="img/en/a/travian0.gif" width="116" height="60" border="0"></a>
+
+<table width="116" cellspacing="0" cellpadding="0">
+<tr>
+<td class="menu">
+<a href="http://www.travian.com/">Homepage</a>
+<a href="#" onClick="Popup(0,0)">Instructions</a>
+<a href="spieler.php?uid=10576">Profile</a><a href="logout.php">Logout</a><br><br>
+<a href="http://forum.travian.com/" target="_blank">Forum</a>
+<a href="http://www.travian.com/chat/" target="_blank">Chat</a>
+<br><br><a href="plus.php?id=2">Travian <b><font color="#71D000">P</font><font color="#FF6F0F">l</font><font color="#71D000">u</font><font color="#FF6F0F">s</font></b></a>
+<!--<a href="mobile.php">Travian <i><font color="#71D000"><b>Mobile</b></font></i></a>-->
+</td>
+</tr>
+</table>
+
+<td class="s2"><img src="img/un/a/w.gif" width="1" height="10" border="0"></td>
+<td class="s3"><div class="dname"><h1>Utopia101</span></h1></div>
+<style><!--
+
+//-->
+</style>
+
+<map name="rx"><area href="build.php?id=1" coords="101,33,28" shape="circle" title="Woodcutter level 8"><area href="build.php?id=2" coords="165,32,28" shape="circle" title="Cropland level 8"><area href="build.php?id=3" coords="224,46,28" shape="circle" title="Woodcutter level 8"><area href="build.php?id=4" coords="46,63,28" shape="circle" title="Iron mine level 7"><area href="build.php?id=5" coords="138,74,28" shape="circle" title="Clay pit level 8"><area href="build.php?id=6" coords="203,94,28" shape="circle" title="Clay pit level 8"><area href="build.php?id=7" coords="262,86,28" shape="circle" title="Iron mine level 7"><area href="build.php?id=8" coords="31,117,28" shape="circle" title="Cropland level 7"><area href="build.php?id=9" coords="83,110,28" shape="circle" title="Cropland level 7"><area href="build.php?id=10" coords="214,142,28" shape="circle" title="Iron mine level 8"><area href="build.php?id=11" coords="269,146,28" shape="circle" title="Iron mine level 7"><area href="build.php?id=12" coords="42,171,28" shape="circle" title="Cropland level 7"><area href="build.php?id=13" coords="93,164,28" shape="circle" title="Cropland level 7"><area href="build.php?id=14" coords="160,184,28" shape="circle" title="Woodcutter level 8"><area href="build.php?id=15" coords="239,199,28" shape="circle" title="Cropland level 8"><area href="build.php?id=16" coords="87,217,28" shape="circle" title="Clay pit level 7"><area href="build.php?id=17" coords="140,231,28" shape="circle" title="Woodcutter level 8"><area href="build.php?id=18" coords="190,232,28" shape="circle" title="Clay pit level 8"><area href="dorf2.php" coords="144,131,36" shape="circle" title="Village center"></map>
+
+<div class="rf0"><img usemap="#rx" src="img/un/g/f3.jpg" width="300" height="264" border="0"><img src="img/un/g/s/s8.gif" class="rf1"><img src="img/un/g/s/s8.gif" class="rf2"><img src="img/un/g/s/s8.gif" class="rf3"><img src="img/un/g/s/s7.gif" class="rf4"><img src="img/un/g/s/s8.gif" class="rf5"><img src="img/un/g/s/s8.gif" class="rf6"><img src="img/un/g/s/s7.gif" class="rf7"><img src="img/un/g/s/s7.gif" class="rf8"><img src="img/un/g/s/s7.gif" class="rf9"><img src="img/un/g/s/s8.gif" class="rf10"><img src="img/un/g/s/s7.gif" class="rf11"><img src="img/un/g/s/s7.gif" class="rf12"><img src="img/un/g/s/s7.gif" class="rf13"><img src="img/un/g/s/s8.gif" class="rf14"><img src="img/un/g/s/s8.gif" class="rf15"><img src="img/un/g/s/s7.gif" class="rf16"><img src="img/un/g/s/s8.gif" class="rf17"><img src="img/un/g/s/s8.gif" class="rf18"></div>
+
+<img class="rf01" usemap="#rx" src="img/un/a/x.gif" width="300" height="267" border="0">
+<div id="att" style="position:absolute; width:230px; height:2px; z-index:4; border: 0px solid #000000; left:455px; top:155px"><div class="f10 b">&nbsp;Troop movements:</div><table class="f10"><tr>
+<td><a href="build.php?gid=16"><img src="img/un/a/att2.gif" width="16" height="16" border="0"></a></td>
+<td><b class="c4 f10" align="right">&laquo; 1</b></td>
+<td><b class="c4 f10">Attack</b></td>
+<td align="center" class="f10">in</td>
+<td class="f10"><span id=timer1>0:34:11</span> h</td>
+</tr></table><p><div class="f10 b">&nbsp;Production:</div><table class="f10">
+<tr>
+<td><img src="img/un/r/1.gif" width="18" height="12" border="0"></td>
+<td>Lumber:</td><td align="right"><b>400&nbsp;</b></td><td> per hour</td>
+</tr>
+<tr>
+<td><img src="img/un/r/2.gif" width="18" height="12" border="0"></td>
+<td>Clay:</td><td align="right"><b>370&nbsp;</b></td><td> per hour</td>
+</tr>
+<tr>
+<td><img src="img/un/r/3.gif" width="18" height="12" border="0"></td>
+<td>Iron:</td><td align="right"><b>310&nbsp;</b></td><td> per hour</td>
+</tr>
+<tr>
+<td><img src="img/un/r/4.gif" width="18" height="12" border="0"></td>
+<td>Crop:</td><td align="right"><b>205&nbsp;</b></td><td> per hour</td>
+</tr>
+
+</table></p><p><div class="f10 b">&nbsp;Troops:</div><table class="f10"><tr><td><a href="build.php?gid=16"><img src="img/un/u/5.gif" width="16" height="16" border="0"></a></td><td align="right">&nbsp;<b>3</b></td><td>Equites Imperatoris</td></tr></table></div></p></td>
+<td class="s2"><img src="img/un/a/w.gif" width="1" height="10" border="0"></td>
+</tr>
+</table><div class="div4">
+<table align="center" cellspacing="0" cellpadding="0"><tr class="f10" valign="top">
+<td><img src="img/un/r/1.gif" width="18" height="12" border="0" title="Lumber"></td>
+<td id=l1 title=400>2378/6300</td>
+<td class="s7"> <img src="img/un/r/2.gif" width="18" height="12" title="Clay"></td>
+<td id=l2 title=370>3588/6300</td>
+<td class="s7"> <img src="img/un/r/3.gif" width="18" height="12" title="Iron"></td>
+<td id=l3 title=310>2491/6300</td><td class="s7"> <img src="img/un/r/4.gif" width="18" height="12" title="Crop"></td>
+<td id=l4 title=205>2541/3100</td>
+<td class="s7"> &nbsp;<img src="img/un/r/5.gif" width="18" height="12" alt="Crop usage">&nbsp;275/480</td></tr></table>
+</div></div><div class="div3">Calculation in <b>14</b> ms<br>Server time: <b><span id=tp1>11:33:40</span></b> <span class="f6">(CET-6)</span></div>
+<div id="ce"></div></body>
+</html>
