09fd866fe2f8b58d290646705bf6a31ed983ae10
YehudaEi <yeisenbe@g.jct.ac.il>
2021-02-18 09:39:35 +0200

    add berichte.php
    From the link: https://www.autoitscript.com/forum/applications/core/interface/file/attachment.php?id=33564

---
+++ Attachments/33K-34K/3000-4000/33564/berichte.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+	<title>Travian it8</title>
+	<meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta http-equiv="expires" content="0" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<script src="mt-full.js?c6ab5" type="text/javascript"></script>
+	<script src="unx.js?c6ab5" type="text/javascript"></script>
+	<script src="new.js?c6ab5" type="text/javascript"></script>
+	        <link href="gpack/travian_basic/lang/it/compact.css?c6ab5" rel="stylesheet" type="text/css" /> 
+<link href="gpack/travian_basic/lang/it/lang.css?c6ab5" rel="stylesheet" type="text/css" /> 
+<link href="gpack/travian_redesign/travian.css?c6ab5" rel="stylesheet" type="text/css" /> 
+<link href="gpack/travian_redesign/lang/it/lang.css?c6ab5" rel="stylesheet" type="text/css" /> 
+        	<script type="text/javascript">
+		window.addEvent('domready', start);
+	</script>
+</head>
+
+
+<body class="v35 gecko">
+
+
+<div class="wrapper">
+<img src="img/x.gif" id="msfilter" alt="" />
+<div id="dynamic_header">
+	</div>
+
+
+<div id="header">
+	<div id="mtop">
+		<a href="dorf1.php" id="n1" accesskey="1"><img src="img/x.gif" title="Panoramica villaggio" alt="Panoramica villaggio" /></a>
+		<a href="dorf2.php" id="n2" accesskey="2"><img src="img/x.gif" title="Centro del Villaggio" alt="Centro del Villaggio" /></a>
+		<a href="karte.php" id="n3" accesskey="3"><img src="img/x.gif" title="Mappa" alt="Mappa" /></a>
+		<a href="statistiken.php" id="n4" accesskey="4"><img src="img/x.gif" title="Statistiche" alt="Statistiche" /></a>
+  		<div id="n5" class="i1">
+			<a class="reports" href="berichte.php" accesskey="5"><img src="img/x.gif" class="l" title="Report" alt="Report"/></a>
+			<a class="messages" href="nachrichten.php" accesskey="6"><img src="img/x.gif" class="r" title="Messaggi" alt="Messaggi" /></a>
+		</div>
+		<a href="plus.php" id="plus"><span class="plus_text">Plus</span><img src="img/x.gif" id="btn_plus" class="inactive" title="Menu Plus" alt="Menu Plus" /></a>
+		<div class="clear"></div>
+	</div>
+</div>
+
+<div id="mid">
+		<div id="side_navi">
+		<a id="logo" href="http://www.travian.it/"><img  src="img/x.gif" alt="Travian" /></a>
+			<p>
+			<a href="http://www.travian.it/">Homepage</a>
+            <a href="#" onclick="return Popup(0,0);">Istruzioni</a>
+			<a href="spieler.php?uid=16920">Profilo</a>
+			<a href="logout.php">Logout</a>
+		</p>
+		<p>
+							<a href="http://forum.travian.it/" target="_blank">Forum</a>
+						
+					</p>
+		<p>
+			<a href="plus.php?id=3">Travian <b><span class="plus_g">P</span><span class="plus_o">l</span><span class="plus_g">u</span><span class="plus_o">s</span></b></a>
+			<a href="support.php"><b><font color="#FF6F0F">-> Supporto <-</font></b></a>
+		</p>
+							</div>
+
+	<div id="content"  class="reports">
+<h1>Report</h1>
+<div id="textmenu">
+   <a href="berichte.php" class="selected ">Tutti</a>
+ | <a href="berichte.php?t=2">Commercio</a>
+ | <a href="berichte.php?t=1">Rinforzo</a>
+ | <a href="berichte.php?t=3">Attacchi</a>
+ | <a href="berichte.php?t=4">Vari</a>
+</div>
+
+			<table cellpadding="1" cellspacing="1" id="report_surround">
+			<thead>
+				<tr>
+					<th>Oggetto:</th>
+					<th>A01 attacca Eburacum</th>
+				</tr>
+
+				<tr>
+					<td class="sent">Inviato:</td>
+					<td>il oggi <span>alle 12:18:50</span><span> </span></td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr><td colspan="2" class="empty"></td></tr>
+				<tr><td colspan="2" class="report_content">
+		<table cellpadding="1" cellspacing="1" id="attacker"><thead>
+<tr>
+<td class="role">Attaccante</td>
+<td colspan="10"><a href="spieler.php?uid=16920">Incubo</a> dal villaggio <a href="karte.php?d=215840&amp;c=fb">A01</a></td>
+</tr>
+</thead>
+<tbody class="units">
+<tr>
+<td>&nbsp;</td><td><img src="img/x.gif" class="unit u11" title="Combattente" alt="Combattente" /></td><td><img src="img/x.gif" class="unit u12" title="Alabardiere" alt="Alabardiere" /></td><td><img src="img/x.gif" class="unit u13" title="Combattente con ascia" alt="Combattente con ascia" /></td><td><img src="img/x.gif" class="unit u14" title="Esploratore" alt="Esploratore" /></td><td><img src="img/x.gif" class="unit u15" title="Paladino" alt="Paladino" /></td><td><img src="img/x.gif" class="unit u16" title="Cavaliere Teutonico" alt="Cavaliere Teutonico" /></td><td><img src="img/x.gif" class="unit u17" title="Ariete" alt="Ariete" /></td><td><img src="img/x.gif" class="unit u18" title="Catapulta" alt="Catapulta" /></td><td><img src="img/x.gif" class="unit u19" title="Comandante" alt="Comandante" /></td><td><img src="img/x.gif" class="unit u20" title="Colono" alt="Colono" /></td></tr><tr><th>Truppe</th><td>8</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td></tr><tr><th>Perdite</th><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td></tr></tbody><tbody class="goods"><tr>
+	<th>Bottino</th>
+	<td colspan="10">
+	<div class="res"><img class="r1" src="img/x.gif" alt="Legno" title="Legno" />58 | <img class="r2" src="img/x.gif" alt="Argilla" title="Argilla" />59 | <img class="r3" src="img/x.gif" alt="Ferro" title="Ferro" />45 | <img class="r4" src="img/x.gif" alt="Grano" title="Grano" />40</div><div class="carry"><img class="car" src="img/x.gif" alt="porta" title="porta" />202/480</div></td></tr></tbody></table><table cellpadding="1" cellspacing="1" class="defender">
+	<thead>
+	<tr>
+	<td class="role">Difensore</th>
+	<td colspan="10"><a href="spieler.php?uid=20557">FireFox</a> dal villaggio <a href="karte.php?d=221400&amp;c=88">Eburacum</a></td>
+	</tr></thead>
+	<tbody class="units">
+	<tr>
+	<td>&nbsp;</td><td><img src="img/x.gif" class="unit u1" title="Legionario" alt="Legionario" /></td><td><img src="img/x.gif" class="unit u2" title="Pretoriano" alt="Pretoriano" /></td><td><img src="img/x.gif" class="unit u3" title="Imperiano" alt="Imperiano" /></td><td><img src="img/x.gif" class="unit u4" title="Emissario a cavallo" alt="Emissario a cavallo" /></td><td><img src="img/x.gif" class="unit u5" title="Cavaliere del Generale" alt="Cavaliere del Generale" /></td><td><img src="img/x.gif" class="unit u6" title="Cavaliere di Cesare" alt="Cavaliere di Cesare" /></td><td><img src="img/x.gif" class="unit u7" title="Ariete" alt="Ariete" /></td><td><img src="img/x.gif" class="unit u8" title="Onagro" alt="Onagro" /></td><td><img src="img/x.gif" class="unit u9" title="Senatore" alt="Senatore" /></td><td><img src="img/x.gif" class="unit u10" title="Colono" alt="Colono" /></td></tr><tr><th>Truppe</th><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td></tr><tr><th>Perdite</th><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td><td class="none">0</td></tr></tbody></table></td></tr></tbody></table></div>
+
+<div id="side_info">
+
+	<table cellpadding="1" cellspacing="1" id="vlist">
+	<thead>
+		<tr>
+			<td colspan="3">
+	<a href="dorf3.php" accesskey="9">Villaggi</a></td>
+		</tr>
+	</thead>
+	<tbody >
+	<tr><td class="dot hl">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=80884&amp;id=15400445|38de7cef">A01</a></div></td><td class="aligned_coords">
+			<div class="cox">(-30</div>
+			<div class="pi">|</div>
+			<div class="coy">131)</div>
+			</td>
+		</tr><tr><td class="dot">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=93864&amp;id=15400445|38de7cef" accesskey="n">B01</a></div></td><td class="aligned_coords">
+			<div class="cox">(-37</div>
+			<div class="pi">|</div>
+			<div class="coy">142)</div>
+			</td>
+		</tr><tr><td class="dot">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=109481&amp;id=15400445|38de7cef">B02</a></div></td><td class="aligned_coords">
+			<div class="cox">(-35</div>
+			<div class="pi">|</div>
+			<div class="coy">142)</div>
+			</td>
+		</tr><tr><td class="dot">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=111029&amp;id=15400445|38de7cef">B03</a></div></td><td class="aligned_coords">
+			<div class="cox">(-40</div>
+			<div class="pi">|</div>
+			<div class="coy">142)</div>
+			</td>
+		</tr><tr><td class="dot">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=112509&amp;id=15400445|38de7cef">B04</a></div></td><td class="aligned_coords">
+			<div class="cox">(-36</div>
+			<div class="pi">|</div>
+			<div class="coy">145)</div>
+			</td>
+		</tr><tr><td class="dot">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=114669&amp;id=15400445|38de7cef">B05</a></div></td><td class="aligned_coords">
+			<div class="cox">(-35</div>
+			<div class="pi">|</div>
+			<div class="coy">139)</div>
+			</td>
+		</tr><tr><td class="dot">&#x25CF;</td>
+			<td class="link"><div><a href="?newdid=116038&amp;id=15400445|38de7cef" accesskey="l">B06</a></div></td><td class="aligned_coords">
+			<div class="cox">(-38</div>
+			<div class="pi">|</div>
+			<div class="coy">139)</div>
+			</td>
+		</tr></tbody>
+</table></div>
+<div class="clear"></div>
+</div>
+
+
+<div class="footer-stopper"></div>
+<div class="clear"></div>
+<div id="footer">
+	<div id="mfoot">
+		<div class="footer-menu">
+			
+			<a href="http://www.travian.it/#screenshots">Immagini</a>
+			| <a href="http://www.travian.it/#spielregeln">Regolamento</a>
+			| <a href="http://www.travian.it/#agb">Termini di Utilizzo</a>
+			| <a href="http://www.travian.it/#impressum">Contatti</a>
+			<br />
+			<div class="copyright">© 2004 - 2011 Travian Games GmbH</div>
+							</div>
+			</div>
+    <div id="cfoot"></div>
+</div>
+
+<div id="res">
+	<div id="resWrap">
+		<table cellpadding="1" cellspacing="1">
+			<tr>
+									<td><img src="img/x.gif" class="r1" alt="Legno" title="Legno" /></td>
+					<td id="l4" title="1250">8289/80000</td>
+									<td><img src="img/x.gif" class="r2" alt="Argilla" title="Argilla" /></td>
+					<td id="l3" title="1500">8482/80000</td>
+									<td><img src="img/x.gif" class="r3" alt="Ferro" title="Ferro" /></td>
+					<td id="l2" title="1250">8299/80000</td>
+									<td><img src="img/x.gif" class="r4" alt="Grano" title="Grano" /></td>
+					<td id="l1" title="207">17355/80000</td>
+														<td><img src="img/x.gif" class="r5" alt="Consumo di grano" title="Consumo di grano" /></td>
+				<td>2793/3000</td>
+			</tr>
+		</table>
+	</div>
+</div>
+
+
+
+<div id="stime">
+	<div id="ltime">
+		<div id="ltimeWrap">
+						Calcolato in <b>52</b> ms
+
+						<br />Ora del server: <span id="tp1" class="b">12:18:54</span>
+
+			
+			
+					</div>
+	</div>
+</div>
+
+<div id="ce"></div>
+</div>
+</body>
+</html>
